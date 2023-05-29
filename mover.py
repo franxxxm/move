649e9ba38c41f4b5dd46b8139a5f97fae9ede704
franxxxm <janjergu@gmail.com>
2023-05-29 10:20:36 -0300

    corrigindo erro

--- mover.py
+++ mover.py
@@ -4,6 +4,7 @@ import os
 diretorio_das_pastas = "C:/Users/Administrador/Desktop/imagem"
 pasta_com_todos_arquivos = 'C:\\Users\\Administrador\\Desktop\\imagem\\img'
 extensao = ['png','jpg','jpeg','gif']
+num = 0
 
 conteudo_do_bat = '@echo off \n'
 for pasta_raiz, a, arquivos in os.walk(diretorio_das_pastas):
@@ -11,8 +12,10 @@ for pasta_raiz, a, arquivos in os.walk(diretorio_das_pastas):
         for tipos in extensao:
             final = arquivo.split(".")[1]
             if(tipos == final):
+                num += 1
                 caminho_completo = os.path.join(pasta_raiz, arquivo)
                 caminho_completo = caminho_completo.replace("/", "\\")
                 conteudo_do_bat += 'move "{}" "{}" \n'.format(caminho_completo, pasta_com_todos_arquivos)
-                with open("movedor.bat", 'w', newline='\r\n') as pontoBAT:
-                    pontoBAT.write(conteudo_do_bat)
+                if(num == 300):
+                    with open("movedor.bat", 'w', newline='\r\n') as pontoBAT:
+                        pontoBAT.write(conteudo_do_bat)
